0cb50ed2daa5f7cceed54d25dc3000ec1d54c519
Zoki Gorgiev <zokigorgiev83@gmail.com>
2017-03-24 00:27:59 +0100

    Image class and image included.

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-// import $ from 'jquery';
 import {Scotch} from '/classes/scotch.js';
 import {Irish} from '/classes/irish.js';
 import {American} from '/classes/american.js';
 import {data} from '/data/data.js';
 import {DataClass} from '/data/data-class.js';
 import {Button} from '/user-interface/button.js';
+import {Image} from '/user-interface/image.js';
 
 let button = new Button('Click Me!');
 button.appendToElement($('body'));
+
+let image = new Image('./images/whiskey.jpg');
+image.appendToElement($('body'));
