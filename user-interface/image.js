807700fbf423a5ead4c0f4980c72a5a9605eeeb3
Zoki Gorgiev <zokigorgiev83@gmail.com>
2017-03-24 00:32:18 +0100

    Slightly changed image.js file

--- user-interface/image.js
+++ user-interface/image.js
@@ -2,14 +2,14 @@ import {BaseElement} from './base-element.js';
 
 export class Image extends BaseElement {
 
-    constructor (fileName) {
+    constructor (imageName) {
         super();
-        this.fileName = fileName;
+        this.imageName = imageName;
     }
 
     getElementString() {
         return `
-                <img src="${this.fileName}" style="width: 100%;" />
+                <img src="${this.imageName}" style="width: 100%;" />
             `;
     }
 }
