11f723fee7fd61e5b852cced6bef77434c1490d0
Zoki Gorgiev <zokigorgiev83@gmail.com>
2017-03-24 19:12:40 +0100

    Title bar and links.

--- app.js
+++ app.js
@@ -5,9 +5,17 @@ import {data} from '/data/data.js';
 import {DataClass} from '/data/data-class.js';
 import {Button} from '/user-interface/button.js';
 import {Image} from '/user-interface/image.js';
+import {TitleBar} from '/user-interface/title-bar.js';
 
-let button = new Button('Click Me!');
-button.appendToElement($('body'));
+// let button = new Button('Click Me!');
+// button.appendToElement($('body'));
 
-let image = new Image('./images/whiskey.jpg');
-image.appendToElement($('body'));
+// let image = new Image('./images/whiskey.jpg');
+// image.appendToElement($('body'));
+
+let titleBar = new TitleBar('Whiskey in the jar!');
+titleBar.addLink('Home', '');
+titleBar.addLink('Scotch', '');
+titleBar.addLink('Irish', '');
+titleBar.addLink('American', '');
+titleBar.appendToElement($('body'));
